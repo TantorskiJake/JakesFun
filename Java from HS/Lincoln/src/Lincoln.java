2bd061293c74d7d118a17e65195477ba3826a090
Jake Tantorski <jetantor@gmail.com>
2020-12-29 16:46:43 -0500

    new

--- Java from HS/Lincoln/src/Lincoln.java
+++ Java from HS/Lincoln/src/Lincoln.java
@@ -2,7 +2,7 @@
 public class Lincoln {
 
 	public static void main(String[] args) {
-		System.out.println("A quote from Abe:\"What ever ypu are be a good one.\"");
+		System.out.println("A quote from Abe:\"Whatever you are be a good one.\"");
 	}
 
 }
--- Java from HS/MPG/src/MPG.java
+++ Java from HS/MPG/src/MPG.java
@@ -3,40 +3,40 @@ import java.util.Scanner;
 public class MPG {
 
 	public static void main(String[] args) {
-		 
+
 			Scanner scan = new Scanner(System.in);
-		 
-				System.out.print("How many miles were on the odometer at the beginning:");
- 
-				double milesstart;
- 
-                 milesstart = scan.nextDouble();
-                 
-                 System.out.print("How many miles were on the odometer at the end: ");
-        		 
-                 double milesend;
- 
-                 milesend = scan.nextDouble();
-                 
-                 double milestotal;
- 
-                 milestotal =milesend - milesstart;
- 
-                 System.out.print("How many gallons were used:");
- 
-                 double gallons;
- 
-                 gallons = scan.nextDouble();
- 
-  
- 
-                 double mpg;
- 
-                 mpg = milestotal / gallons;
- 
-  
- 
-                 System.out.println("The Miles-Per-Gallon used in this trip are: " + mpg + "!");
+
+			 System.out.print("How many miles were on the odometer at the beginning:");
+
+			 double milesstart;
+
+       milesstart = scan.nextDouble();
+
+       System.out.print("How many miles were on the odometer at the end: ");
+
+       double milesend;
+
+       milesend = scan.nextDouble();
+
+       double milestotal;
+
+       milestotal = milesend - milesstart;
+
+       System.out.print("How many gallons were used:");
+
+       double gallons;
+
+       gallons = scan.nextDouble();
+
+
+
+       double mpg;
+
+       mpg = milestotal / gallons;
+
+
+
+       System.out.println("The Miles-Per-Gallon used in this trip are: " + mpg + "!");
 
 	}
 
