015603d17f91c5e9dd1740c1da015b5a1c02e804
Jake Tantorski <31451263+TantorskiJake@users.noreply.github.com>
2021-08-22 22:00:35 -0400

    Cleaned Code for Fibonacci

--- PythonExamples/Fibonacci.py
+++ PythonExamples/Fibonacci.py
@@ -3,17 +3,18 @@
 # Coding the Fibonacci Sequence for any number
 
 def Fibonacci(n):
-    if n<0:
+    if n < 0:
         print("Incorrect input")
     # First Fibonacci number is 0
-    elif n==0:
+    elif n == 0:
         return 0
     # Second Fibonacci number is 1
-    elif n==1:
+    elif n == 1:
         return 1
     else:
-        return Fibonacci(n-1)+Fibonacci(n-2)
+        return Fibonacci(n - 1) + Fibonacci(n - 2)
+
 
 num = int(input('Enter Fibonacci Index: '))
 output = Fibonacci(num)
-print("Number: "+ str(output))
+print("Number: " + str(output))
