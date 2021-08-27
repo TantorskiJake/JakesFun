c907288f73fafce279bef73b7e6ee6cf4be0652d
Jake Tantorski <31451263+TantorskiJake@users.noreply.github.com>
2021-08-26 20:43:27 -0400

    Added Sum of List Program

---
+++ PythonExamples/SumList.py
@@ -0,0 +1,23 @@
+# Jake Tantorski August 26th, 2021
+
+# Coding the Sum of List
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9]
+
+# Recursive Way
+def sumOfList(list, size):
+    if (size == 0):
+        return 0
+    else:
+        return list[size - 1] + sumOfList(list, size - 1)
+
+
+total1 = sumOfList(list1, len(list1))
+
+# Sum Function Way
+total2 = sum(list2)
+
+print("Sum of List: ", total1)
+
+print("Sum of List: ", total2)
